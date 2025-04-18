c37d3abafc9461e2b3b5de934f125c716906ba30
bantunagarjuna <bantunagarjuna2@gmail.com>
2025-04-18 13:49:30 +0530

    first commit2

--- stock_prediction_main/wsgi.py
+++ stock_prediction_main/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
+settings_module = 'stock_prediction_main.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'stock_prediction_main.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
+
 
 application = get_wsgi_application()
